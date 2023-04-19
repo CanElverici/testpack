b2f151064803a8fd44e25990b6db2f398cca03fb
CanElverici <elverici.can@gmail.com>
2023-04-19 16:43:01 -0500

    v0.0.2 update

--- R/f1.R
+++ R/f1.R
@@ -1,12 +1,12 @@
 #' Add F1 to Your Text
 #'
-#' @param text A string
+#' @param text (character) the text to paste with "F1". Default = "default"
 #'
-#' @return A string with F1 added
+#' @return A character vector of length 1.
 #' @export
 #'
 #' @examples
-#' f1(text = "web")
+#' F1 <- f1(text = "web")
 f1 <- function(text = "default") {
   return(paste(text, "F1"))
 }
