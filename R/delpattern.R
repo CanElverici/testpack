5a5215d4badfd85bf6912c037a9204da854badeb
CanElverici <elverici.can@gmail.com>
2023-04-19 16:23:05 -0500

    updated code

--- R/delpattern.R
+++ R/delpattern.R
@@ -1,6 +1,20 @@
-delpattern <- function(directory, pattern) {
-  # List all files in the directory and its subdirectories
-  files <- list.files(directory, pattern = pattern, full.names = TRUE, recursive = TRUE)
+#' Delete files with matching names
+#'
+#' @param directory A directory to process which includes files
+#' @param pattern A pattern that will be removed
+#' @param recursive A logical statement if the function will work recursively or not
+#'
+#' @return A vector of deleted files
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' delete_files_matching_pattern(tempdir(), "test")
+#' delete_files_matching_pattern(tempdir(), "test", recursive = TRUE)
+#' }
+delpattern <- function(directory, pattern, recursive = FALSE) {
+  # List all files in the directory and, if specified, its subdirectories
+  files <- list.files(directory, pattern = pattern, full.names = TRUE, recursive = recursive)
 
   # Delete matching files
   deleted_files <- files[file.remove(files)]
